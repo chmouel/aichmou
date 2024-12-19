0ea0a916ba4e22bf4947d86437119178d30b2d69
Chmouel Boudjnah <chmouel@chmouel.com>
2024-12-19 22:51:06 +0100

    iadd gemini

---
+++ aichmou/gemini.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# Author: Chmouel Boudjnah <chmouel@chmouel.com>
+
+import google.generativeai as genai
+
+from aichmou.common import get_pass_key
+
+from .abstract_ai import AbstractAI
+
+
+class Gemini(AbstractAI):
+    model = "gemini-1.5-flash"
+
+    def complete(self, prompt: str, text: str) -> str:
+        api_key = self.api_key
+        if self.api_key.startswith("pass::"):
+            api_key = get_pass_key(self.api_key.split("::")[1])
+        genai.configure(api_key=api_key)
+        model = genai.GenerativeModel(self.model)
+        response = model.generate_content(prompt + text)
+        return response.text
